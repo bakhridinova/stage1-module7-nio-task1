eba9a25f10a48cb801271c47d920d5078ec547fd
bakhridinova <bakhridinovaleyla@gmmail.com>
2023-01-24 22:14:42 +0500

    updated FileReader.java

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,50 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.List;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = null;
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(file.getAbsoluteFile(), "r");
+             FileChannel fileChannel = randomAccessFile.getChannel()) {
+            ByteBuffer byteBuffer = ByteBuffer.allocate((int) randomAccessFile.length());
+            StringBuilder stringBuilder = new StringBuilder();
+            while (fileChannel.read(byteBuffer) > 0) {
+                byteBuffer.flip();
+                for (int i = 0; i < byteBuffer.limit(); i++) {
+                    stringBuilder.append((char) byteBuffer.get());
+                }
+                byteBuffer.clear();
+            }
+            System.out.println(stringBuilder);
+            List<String> list = List.of(stringBuilder.toString().split("\r\n"));
+            String name = "";
+            int age = 0;
+            String email = "";
+            long phone = 0L;
+            for (String s : list) {
+                int delimiter = s.indexOf(":");
+                if (s.contains("Name")) {
+                    name = s.substring(delimiter + 2);
+                } else if (s.contains("Age")) {
+                    age = Integer.parseInt(s.substring(delimiter + 2));
+                } else if (s.contains("Email")) {
+                    email = s.substring(delimiter + 2);
+                } else {
+                    phone = Long.parseLong(s.substring(delimiter + 2));
+                }
+            }
+            profile = new Profile(name, age, email, phone);
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
